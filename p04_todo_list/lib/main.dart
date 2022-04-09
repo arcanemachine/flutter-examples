c852720cbdcfccf761788954d72360a4cf4b9157
Nicholas Moen <arcanemachine@tutanota.com>
2022-04-09 17:38:19 -0600

    Can now delete items

--- p04_todo_list/lib/main.dart
+++ p04_todo_list/lib/main.dart
@@ -16,56 +16,132 @@ class TodoForm extends StatefulWidget {
 class _TodoFormState extends State<TodoForm> {
   // todos
   final _todos = [];
-  // ignore: unused_field, prefer_final_fields, avoid_init_to_null
   int _activeItemIndex = -1;
 
   // text field
   var _textValue = "";
 
-  final _controller = TextEditingController.fromValue(
+  final _textFieldController = TextEditingController.fromValue(
     const TextEditingValue(text: ""),
   );
+  late FocusNode _textFieldFocusNode;
 
+  // lifecycle methods
   @override
   void initState() {
     super.initState();
-    _controller.addListener(() {
+
+    // controller
+    _textFieldController.addListener(() {
       setState(() {
-        _textValue = _controller.text;
+        _textValue = _textFieldController.text;
       });
     });
+
+    // focus node
+    _textFieldFocusNode = FocusNode();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _textFieldController.dispose();
+    _textFieldFocusNode.dispose();
     super.dispose();
   }
 
-  void _textSet(value) {
+  @override
+  Widget build(BuildContext context) {
+    return _todoList();
+  }
+
+  // widgets
+  Widget _textSection() {
+    return TextField( // text input
+      controller: _textFieldController,
+      focusNode: _textFieldFocusNode,
+      // autofocus: true,
+      decoration: const InputDecoration(
+        border: UnderlineInputBorder(),
+        hintText: "Enter a todo item",
+      ),
+      onSubmitted: (x) => _confirmButtonPressed(),
+    );
+  }
+
+  void _setText(value) {
     setState(() {
       _textValue = value;
-      _controller.text = value;
+      _textFieldController.text = value;
     });
   }
 
-  // add/update button
+  Widget _buttonSection() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+            ),
+            child: Text(_activeItemIndex != -1 ? "Update" : "Add"),
+            onPressed:_textValue != "" ? _confirmButtonPressed : null,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: _activeItemIndex == -1 ? const Text('') : ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              ),
+              child: const Text("Delete"),
+              onPressed: () => _deleteButtonPressed(_activeItemIndex),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   void _confirmButtonPressed() {
+    _textFieldFocusNode.requestFocus();
+
     if (_activeItemIndex == -1) {
-      setState(() {
-        _todos.add(_textValue);
-      });
-      _textSet("");
+      // check for duplicates
+      if (_todos.contains(_textValue)) {
+        // show warning
+        final snackBar = SnackBar(
+          content: const Text("Duplicate items not allowed"),
+          action: SnackBarAction(
+            label: "OK",
+            onPressed: () {},
+          ),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      } else {
+        // add item
+        setState(() {
+          _todos.add(_textValue);
+        });
+
+        // reset the text field
+        _setText("");
+      }
+
     } else {
+      // update item
       setState(() {
         _todos[_activeItemIndex] = _textValue;
         _activeItemIndex = -1;
       });
 
+      // reset the text field
+      _setText("");
     }
   }
 
-  // edit button
   void _editButtonPressed(itemIndex) {
     // if no item is active, then enable the current one.
     // otherwise, set active to null
@@ -73,43 +149,68 @@ class _TodoFormState extends State<TodoForm> {
       setState(() {
         _activeItemIndex = itemIndex;
       });
-      _textSet(_todos[itemIndex]);
-      // todo: focus the text input
+      _setText(_todos[itemIndex]);
+      _textFieldFocusNode.requestFocus();
     } else {
       setState(() {
         _activeItemIndex = -1;
       });
-      _textSet("");
+      _setText("");
     }
   }
 
-  Widget _todoList() {
-    return Flexible(
-      child: ListView.builder( // item list
-        itemCount: _todos.length,
-        itemBuilder: (context, i) {
-          return ListTile(
-            title: Row(
+  void _deleteButtonPressed(itemIndex) {
+    setState(() {
+      _todos.removeAt(itemIndex);
+      _activeItemIndex = -1;
+    });
+    _setText("");
+
+    // show message
+    final snackBar = SnackBar(
+      content: const Text("Item removed"),
+      action: SnackBarAction(
+        label: "OK",
+        onPressed: () {},
+      ),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Widget _todoListItems() {
+    if (_todos.isEmpty) {
+      // if the list is empty, display a message
+      return const Center(
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: Text('You have no todo items.'),
+        ),
+      );
+    } else {
+      // display non-empty list
+      return Flexible(
+        child: ListView.builder( // item list
+          itemCount: _todos.length,
+          itemBuilder: (context, i) {
+            return Row(
               children: [
                 Flexible(
                   child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 2.0),
-                    ),
-                    child: _todoListItem(i, '- ${_todos[i]}'),
+                    child: _todoListItem(i, _todos[i]),
                   ),
                 ),
               ],
-            ),
-          );
-        }
-      ),
-    );
+            );
+          }
+        ),
+      );
+    }
   }
 
   Widget _todoListItem(int itemIndex, var itemText) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(12),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -123,39 +224,26 @@ class _TodoFormState extends State<TodoForm> {
   Widget _editButton(int itemIndex) {
     return IconButton(
       icon: (_activeItemIndex == itemIndex
-        ? const Icon(Icons.cancel)
-        : const Icon(Icons.edit)),
+        ? const Icon(
+            Icons.cancel,
+            semanticLabel: "Cancel",
+          )
+        : const Icon(
+            Icons.edit,
+            semanticLabel: "Edit",
+      )),
       onPressed: () => _editButtonPressed(itemIndex),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _todoList() {
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
-        children: <Widget>[
-          TextField( // text input
-            controller: _controller,
-            autofocus: true,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              hintText: "Enter a todo item",
-            ),
-            // onSubmitted: (value) => _confirmButtonPressed,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: ElevatedButton(
-              // todo: make button bigger
-              // style: const ButtonStyle(
-              //   padding: EdgeInsets.all(4),
-              // ),
-              onPressed:_textValue != "" ? _confirmButtonPressed : null,
-              child: Text(_activeItemIndex != -1 ? "Update" : "Add"),
-            ),
-          ),
-          _todoList(),
+        children: [
+          _textSection(),
+          _buttonSection(),
+          _todoListItems(),
         ],
       ),
     );
@@ -172,8 +260,8 @@ class MyApp extends StatelessWidget {
       title: "Hello world!",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('App Bar'),
-          backgroundColor: Colors.green,
+          title: const Text('Todo List'),
+          backgroundColor: Colors.blue,
         ),
         body: const TodoForm(),
       ),
