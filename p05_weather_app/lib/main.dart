5402348073d03b8c49decb58fca468d232cc48ff
Nicholas Moen <arcanemachine@tutanota.com>
2022-04-12 21:42:52 -0600

    Pass database down to views

--- p05_weather_app/lib/main.dart
+++ p05_weather_app/lib/main.dart
@@ -1,16 +1,16 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-import 'package:p05_weather_app/examples.dart';
+// import 'package:p05_weather_app/examples.dart';
 // import 'package:p05_weather_app/models.dart';
 
 
-Future main() async {
+Future<Database> databaseGetOrCreate() async {
   // platform-specific boilerplate
   if (Platform.isWindows || Platform.isLinux) {
     sqfliteFfiInit(); // initialize FFI
@@ -35,7 +35,82 @@ Future main() async {
     version: 1,
   );
 
-  if (kDebugMode) {
-    runExamples(db);
+  // if (kDebugMode) {
+  //   runExamples(db);
+  // }
+
+  return db;
+}
+
+Future main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _appTitle = "Flutter Weather";
+  late Database db;
+
+  @override
+  void initState() {
+    super.initState();
+
+    asyncInitDb().then((response) {
+      setState(() {
+        db = response;
+      });
+    });
+  }
+
+  asyncInitDb<Database>() async {
+    return databaseGetOrCreate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _appTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeView(
+        title: _appTitle,
+        db: db,
+      ),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  const HomeView({
+    Key? key,
+    required this.title,
+    required this.db,
+  }) : super(key: key);
+
+  final String title;
+  final Database db;
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("Database is ${widget.db.isOpen ? "open" : "closed"}!"),
+      ),
+    );
   }
 }
