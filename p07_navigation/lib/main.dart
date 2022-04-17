de31ee6ed24b179bede878d8cf9f4782a0344467
Nicholas Moen <arcanemachine@tutanota.com>
2022-04-17 15:50:26 -0600

    Add example: Navigation (using go_router)

---
+++ p07_navigation/lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp.router(
+    routeInformationParser: _router.routeInformationParser,
+    routerDelegate: _router.routerDelegate,
+    title: 'GoRouter Example',
+  );
+
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) =>
+          const IntroScreen(),
+      ),
+      GoRoute(
+        path: '/counter',
+        builder: (BuildContext context, GoRouterState state) =>
+          const CounterScreen(title: 'Counter'),
+      ),
+    ],
+  );
+
+  // return MaterialApp(
+  //   title: 'Flutter Demo',
+  //   theme: ThemeData(
+  //     primarySwatch: Colors.blue,
+  //   ),
+  //   home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  // );
+}
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(title: const Text("Intro View")),
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            child: const Text("Show the Counter"),
+            onPressed: () => context.go('/counter'),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  child: const Text("Return to the intro page"),
+                  onPressed: () => context.go('/'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
